7efb447ce03dac68293dfef17b251236af815304
xenon <pmcruz@hermenautas.com>
2025-02-07 12:55:42 +0100

    improved typographic styles and login and logout actions

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import { auth, signOut, signIn } from "@/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+  const options = { redirectTo:'/'};
+  const provider = 'github';
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
-      <nav className="flex justify-between items-center">Navbar</nav>
-    </header>
-  )
-}
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.svg" alt="Logo" width={144} height={30} />
+        </Link>
 
-export default Navbar
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+
+              <form action={async () => 
+                {
+                  "use server";
+                  await signOut(options);
+                }
+              }>
+                <button type="submit">
+                  Logout                
+                </button>
+              </form>
+
+              <Link href={`/user/${session?.user?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form action={async () => 
+              {
+                "use server";
+                await signIn(provider);
+              }
+            }>
+              <button type="submit">
+                Login                
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
