579e521d12a2de9c4c336206ac3594d7144d35bf
xenon <pmcruz@hermenautas.com>
2025-02-10 19:22:36 +0100

    New style for the header and home page headings

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,10 @@ export default function Home() {
     <>
       <section className="pink_container">
         <h1 className="heading">Pitch your startup, connect with entrepreneurs</h1>
-      </section>
-      
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, adn Get Noticed in virtual competitions.
+        </p>
+      </section>     
     </>
   );
 }
